07333a6fc724ab3fddc5038a797ff063414f3368
ali raza <alighuman486@gmail.com>
2023-09-17 22:08:00 +1000

    Initial Commit

---
+++ hh/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hh
+//
+//  Created by ali raza on 17/9/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
